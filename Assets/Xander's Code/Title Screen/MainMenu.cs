5ab8f9c01d134b8ec6455c49202b31de15600309
Redrar <Redra100@gmail.com>
2021-01-11 10:23:50 -0800

    Add files via upload

---
+++ Assets/Xander's Code/Title Screen/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button playButton;
+
+    public Button creditsButton;
+
+    public Button exitGameButton;
+
+    public string CaveLevel;
+
+    public string Credits;
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(CaveLevel);
+
+    }
+
+    public void CreditsButton()
+    {
+        SceneManager.LoadScene(Credits);
+
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+
+    }
+}
