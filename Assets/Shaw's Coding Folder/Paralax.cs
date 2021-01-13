e4a1743cf6067d5bbe60f64598760bee8af66967
circumvention21 <Fleshh13>
2021-01-13 14:02:53 -0800

    Update lvl2.unity, Background Q2 Project Shading trans.png, and 18 more files...

---
+++ Assets/Shaw's Coding Folder/Paralax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    public float length, startpos;
+    public GameObject cam;
+    public float ParallaxEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float distance = (cam.transform.position.x * ParallaxEffect);
+        transform.position = new Vector3(startpos + distance, transform.position.y, transform.position.z);
+    }
+}
